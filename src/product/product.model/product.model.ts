c3f4685ddf482adbb844df8a8a71616c86242b19
soundmafi <soundmafi@gmail.com>
2023-02-28 18:29:32 +0300

    add(models): define peoduct model

--- src/product/product.model/product.model.ts
+++ src/product/product.model/product.model.ts
@@ -1,17 +1,48 @@
-export class ProductModel {
-  _id: string;
+import { prop } from '@typegoose/typegoose';
+import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+export interface AuthModel extends Base {}
+class ProductCharacteristic {
+  @prop()
+  name: string;
+
+  @prop()
+  value: string;
+}
+
+export class ProductModel extends TimeStamps {
+  @prop()
   image: string;
+
+  @prop()
   title: string;
+
+  @prop()
   price: number;
+
+  @prop()
   oldPrrice: number;
+
+  @prop()
   credit: number;
+
+  @prop()
   calculateRating: number;
+
+  @prop()
   description: string;
+
+  @prop()
   advantages: string;
+
+  @prop()
   disAdvantages: string;
+
+  @prop({ type: () => [String] })
   categories: string[];
-  tags: string;
-  characteristics: {
-    [key: string]: string;
-  };
+
+  @prop({ type: () => [String] })
+  tags: string[];
+
+  @prop({ type: () => [ProductCharacteristic], _id: false })
+  characteristics: ProductCharacteristic[];
 }
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductController } from './product.controller';
+import { ProductModel } from './product.model/product.model';
 
 @Module({
-  controllers: [ProductController]
+  controllers: [ProductController],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Product',
+        schema: ProductModel,
+        collection: 'Product',
+      },
+    ]),
+  ],
 })
 export class ProductModule {}
