f226b58673a9841a1348cafabee9097c678cb2a3
soundmafi <soundmafi@gmail.com>
2023-02-28 18:06:38 +0300

    add(models): define auth model

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
+import { AuthModel } from './auth.model/auth.model';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Auth',
+        schema: AuthModel,
+        collection: 'Auth',
+      },
+    ]),
+  ],
 })
 export class AuthModule {}
