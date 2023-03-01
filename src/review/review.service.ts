b917ecbcfa09d3233d376c478a9eba4c31bfabf2
soundmafi <soundmafi@gmail.com>
2023-03-01 16:12:26 +0300

    add(test): add tests for review requests

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -21,8 +21,10 @@ export class ReviewService {
   }
 
   async findByProductId(productId: string): Promise<ReviewDocument[]> {
-    return this.reviewModel
-      .find({ productId: new Types.ObjectId(productId) })
-      .exec();
+    return this.reviewModel.find({ productId }).exec();
+  }
+
+  async deleteByProductId(productId: string): Promise<any> {
+    return this.reviewModel.deleteMany({ productId }).exec();
   }
 }
